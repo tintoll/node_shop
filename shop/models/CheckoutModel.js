264e84aa05bf98002e2b3216e108b268d6bc37df
belselios@gmail.com <belselios@gmail.com>
2019-04-09 05:42:42 +0900

    pg connection

---
+++ shop/models/CheckoutModel.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var { autoIncrement } = require("mongoose-plugin-autoinc");
+
+var CheckoutSchema = new Schema({
+  imp_uid: String, // 고유 ID
+  merchant_uid: String, // 상점 거래 ID
+  paid_amount: Number, // 결재 금액
+  apply_num: String, // 카드 승인번호
+
+  buyer_email: String, // 이메일
+  buyer_name: String,
+  buyer_tel: String,
+  buyer_addr: String,
+  buyer_postcode: String, // 우편번호
+
+  status: String, // 결재완료, 배송중 등
+  song_jang: String, // 송장번호
+  create_at: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+CheckoutSchema.plugin(autoIncrement, {
+  model: "checkout",
+  fiedl: "id",
+  startAt: 1
+});
+module.exports = mongoose.model("checkout", CheckoutSchema);
--- shop/routes/checkout.js
+++ shop/routes/checkout.js
@@ -1,19 +1,67 @@
-var express = require('express');
+var express = require("express");
 var checkoutRouter = express.Router();
 
-checkoutRouter.get('/', (req, res) => {
+var CheckoutModel = require("../models/CheckoutModel");
 
+checkoutRouter.get("/", (req, res) => {
   var totalAmount = 0;
   var cartList = {};
-  if (typeof (req.cookies.cartList) !== 'undefined') {
+  if (typeof req.cookies.cartList !== "undefined") {
     cartList = JSON.parse(unescape(req.cookies.cartList));
     for (var key in cartList) {
       totalAmount += parseInt(cartList[key].amount);
     }
   }
 
-  res.render('checkout/index', { cartList: cartList, totalAmount: totalAmount });
+  res.render("checkout/index", {
+    cartList: cartList,
+    totalAmount: totalAmount
+  });
+});
 
-})
+checkoutRouter.post("/complete", (req, res) => {
+  var checkout = new CheckoutModel({
+    imp_uid: req.body.imp_uid,
+    merchant_uid: req.body.merchant_uid,
+    paid_amount: req.body.paid_amount,
+    apply_num: req.body.apply_num,
 
+    buyer_email: req.body.buyer_email,
+    buyer_name: req.body.buyer_name,
+    buyer_tel: req.body.buyer_tel,
+    buyer_addr: req.body.buyer_addr,
+    buyer_postcode: req.body.buyer_postcode,
+
+    status: req.body.status
+  });
+
+  checkout.save(err => {
+    res.send({ message: "success" });
+  });
+});
+
+checkoutRouter.post("/mobile_complete", (req, res) => {
+  var checkout = new CheckoutModel({
+    imp_uid: req.body.imp_uid,
+    merchant_uid: req.body.merchant_uid,
+    paid_amount: req.body.paid_amount,
+    apply_num: req.body.apply_num,
+
+    buyer_email: req.body.buyer_email,
+    buyer_name: req.body.buyer_name,
+    buyer_tel: req.body.buyer_tel,
+    buyer_addr: req.body.buyer_addr,
+    buyer_postcode: req.body.buyer_postcode,
+
+    status: req.body.status
+  });
+
+  checkout.save(err => {
+    res.send({ message: "success" });
+  });
+});
+
+checkoutRouter.get("/success", (req, res) => {
+  res.render("checkout/success");
+});
 module.exports = checkoutRouter;
